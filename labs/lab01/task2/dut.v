0e12b789da47b7cc44b97ab18869473299b633ba
Shivam Ghadge <f20251368@pilani.bits-pilani.ac.in>
2026-08-29 11:36:11 +0000

    lab01 task2

--- labs/lab01/task2/dut.v
+++ labs/lab01/task2/dut.v
@@ -6,8 +6,6 @@ module dut (
   input  wire b,
   output wire y
 );
-
-  // TODO: Implement OR gate
   // Hint: Use continuous assignment (assign) with the Boolean OR operator (|)
   assign y = a | b;
 endmodule
